0fa323456e6130cde707b24c3345d2692ed57c72
Moritz Löchner <momo.loechner@gmail.com>
2022-02-25 13:02:57 +0100

    nginx setup https

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(app.wunderpass.org)
+domains=(pool.wunderpass.org)
 rsa_key_size=4096
 data_path="./certbot"
 email="m.loechner@wunderpass.io" # Adding a valid address is strongly recommended
