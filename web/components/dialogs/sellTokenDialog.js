89b64441bb1e973151dca7c5486fff6bf089b91b
Moritz Löchner <momo.loechner@gmail.com>
2022-04-16 03:01:00 +0200

    Swap Token Dialog uses new token Input

--- web/components/dialogs/sellTokenDialog.js
+++ web/components/dialogs/sellTokenDialog.js
@@ -1,9 +1,31 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, InputAdornment, LinearProgress, Stack, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  InputAdornment,
+  LinearProgress,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { createFudSuggestion } from "/services/contract/proposals";
 
 export default function SellTokenDialog(props) {
-  const {open, setOpen, name, address, symbol, balance, poolAddress, fetchProposals, handleError, handleSuccess} = props;
+  const {
+    open,
+    setOpen,
+    name,
+    address,
+    symbol,
+    balance,
+    poolAddress,
+    fetchProposals,
+    handleError,
+    handleSuccess,
+  } = props;
   const [amount, setAmount] = useState("");
   const [loading, setLoading] = useState(false);
 
@@ -11,42 +33,74 @@ export default function SellTokenDialog(props) {
     setAmount("");
     setLoading(false);
     setOpen(false);
-  }
+  };
 
   const handleSubmit = () => {
     setLoading(true);
-    createFudSuggestion(poolAddress, address, `Let's Sell ${name} (${symbol})`, `We will sell ${amount} ${symbol}`, amount).then((res) => {
-      console.log(res);
-      handleSuccess(`Created Proposal to sell ${name}`)
-      fetchProposals();
-      handleClose();
-    }).catch((err) => {
-      handleError(err);
-    }).then(() => {
-      setLoading(false)
-    });
-  }
+    createFudSuggestion(
+      poolAddress,
+      address,
+      `Let's Sell ${name} (${symbol})`,
+      `We will sell ${amount} ${symbol}`,
+      amount
+    )
+      .then((res) => {
+        console.log(res);
+        handleSuccess(`Created Proposal to sell ${name}`);
+        fetchProposals();
+        handleClose();
+      })
+      .catch((err) => {
+        handleError(err);
+      })
+      .then(() => {
+        setLoading(false);
+      });
+  };
 
   const handleInput = (e) => {
     setAmount(e.target.value);
-  }
+  };
 
-  return(
-    <Dialog open={open} onClose={handleClose}>
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
       <DialogTitle>Sell {name}</DialogTitle>
       <DialogContent>
-        <TextField autoFocus type="number" margin="dense" value={amount} onChange={handleInput} label="Amount" placeholder="1" fullWidth InputProps={{endAdornment: <InputAdornment position="end"><Button onClick={() => setAmount(balance)}>MAX</Button></InputAdornment>}}/>
+        <TextField
+          autoFocus
+          type="number"
+          margin="dense"
+          value={amount}
+          onChange={handleInput}
+          label="Amount"
+          placeholder="1"
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Button onClick={() => setAmount(balance)}>MAX</Button>
+              </InputAdornment>
+            ),
+          }}
+        />
       </DialogContent>
-      {loading ? 
-        <Stack spacing={2} sx={{textAlign: 'center'}}>
+      {loading ? (
+        <Stack spacing={2} sx={{ textAlign: "center" }}>
           <Typography variant="subtitle1">Creating your Proposal...</Typography>
           <LinearProgress />
-        </Stack> :
+        </Stack>
+      ) : (
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSubmit} color="success" disabled={Number(amount) > Number(balance)}>Sell</Button>
+          <Button
+            onClick={handleSubmit}
+            color="success"
+            disabled={Number(amount) > Number(balance)}
+          >
+            Sell
+          </Button>
         </DialogActions>
-      }
+      )}
     </Dialog>
-  )
+  );
 }
--- web/services/contract/init.js
+++ web/services/contract/init.js
@@ -1,18 +1,32 @@
 import { ethers } from "ethers";
 
-export const httpProvider = new ethers.providers.AlchemyProvider(process.env.BLOCKCHAIN_NAME, process.env.ALCHEMY_API_KEY);
-export const wsProvider = ethers.providers.AlchemyProvider.getWebSocketProvider(process.env.BLOCKCHAIN_NAME, process.env.ALCHEMY_API_KEY);
-export const wunderSwapperAddress = '0xB2BfcfA4d937ac850edCBeC7BaeC7A1b68f2ccfd';
-export const usdcAddress = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
-export const tokenAbi = ["function name() public view returns(string)", "function symbol() public view returns(string)", "function decimals() public view returns(uint)", "function balanceOf(address) public view returns(uint)", "function totalSupply() public view returns(uint)", "function price() public view returns(uint)"];
+export const httpProvider = new ethers.providers.AlchemyProvider(
+  process.env.BLOCKCHAIN_NAME,
+  process.env.ALCHEMY_API_KEY
+);
+export const wsProvider = ethers.providers.AlchemyProvider.getWebSocketProvider(
+  process.env.BLOCKCHAIN_NAME,
+  process.env.ALCHEMY_API_KEY
+);
+export const wunderSwapperAddress =
+  "0xB2BfcfA4d937ac850edCBeC7BaeC7A1b68f2ccfd";
+export const usdcAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+export const tokenAbi = [
+  "function name() public view returns(string)",
+  "function symbol() public view returns(string)",
+  "function decimals() public view returns(uint)",
+  "function balanceOf(address) public view returns(uint)",
+  "function totalSupply() public view returns(uint)",
+  "function price() public view returns(uint)",
+];
 
 export function initLauncher() {
-  const address = "0xAE32217Dc2d87c07C0885D69121B968C96d3E693"
+  const address = "0xAE32217Dc2d87c07C0885D69121B968C96d3E693";
   const abi = [
     "function createNewPool(string _poolName, uint256 _entryBarrier, string _tokenName, string _tokenSymbol, uint256 _invest)",
     "function allPools() view returns (address[])",
-    "function poolsOfMember(address _member) view returns (address[])"
-  ]
+    "function poolsOfMember(address _member) view returns (address[])",
+  ];
   const provider = httpProvider;
   return [new ethers.Contract(address, abi, provider), provider];
 }
@@ -31,13 +45,13 @@ export function initPool(poolAddress) {
     "function fundPool(uint amount) external",
     "function createProposal(string memory _title, string memory _description, address _contractAddress, string memory _action, bytes memory _param, uint _transactionValue, uint _deadline) public",
     "function createMultiActionProposal(string memory _title, string memory _description, address[] memory _contractAddresses, string[] memory _actions, bytes[] memory _params, uint[] memory _transactionValues, uint _deadline) public",
-    "function getAllProposalIds() public view returns(uint[] memory)", 
+    "function getAllProposalIds() public view returns(uint[] memory)",
     "function getProposal(uint _proposalId) public view returns(string memory title, string memory description, uint transactionCount, uint deadline, uint yesVotes, uint noVotes, uint totalVotes, uint createdAt, bool executed)",
     "function getProposalTransaction(uint _proposalId, uint _transactionIndex) public view returns(string memory action, bytes memory param, uint transactionValue, address contractAddress)",
     "function vote(uint proposalId, uint mode) public",
     "function hasVoted(uint256 proposalId, address account) public view returns (uint)",
-    "function executeProposal(uint _proposalId) public"
-  ]
+    "function executeProposal(uint _proposalId) public",
+  ];
   const provider = httpProvider;
   return [new ethers.Contract(poolAddress, abi, provider), provider];
 }
@@ -50,9 +64,9 @@ export function initPoolSocket(poolAddress) {
     "event NewMember(address indexed memberAddress, uint stake)",
     "event TokenAdded(address indexed tokenAddress, bool _isERC721, uint _tokenId)",
     "event MaticWithdrawed(address indexed receiver, uint amount)",
-    "event TokensWithdrawed(address indexed tokenAddress, address indexed receiver, uint amount)"
-  ]
-  
+    "event TokensWithdrawed(address indexed tokenAddress, address indexed receiver, uint amount)",
+  ];
+
   const provider = wsProvider;
   const wunderPool = new ethers.Contract(poolAddress, abi, provider);
 
