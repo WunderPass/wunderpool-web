c11996d4147de3cfc7fee38e0a98ee9570408db5
Moritz Löchner <momo.loechner@gmail.com>
2022-04-21 13:03:21 +0200

    Fixes #7 Refresh Pool wenn USer gejoined

--- web/components/dialogs/joinPool.js
+++ web/components/dialogs/joinPool.js
@@ -1,4 +1,16 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, InputAdornment, LinearProgress, Stack, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  InputAdornment,
+  LinearProgress,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { ethers } from "ethers";
 import { useState } from "react";
 import { toEthString } from "/services/formatter";
@@ -6,7 +18,17 @@ import { joinPool } from "/services/contract/pools";
 import { usdc } from "../../services/formatter";
 
 export default function JoinPoolDialog(props) {
-  const {open, setOpen, address, handleSuccess, handleError, price, totalSupply, minimumInvest} = props;
+  const {
+    open,
+    setOpen,
+    loginCallback,
+    address,
+    handleSuccess,
+    handleError,
+    price,
+    totalSupply,
+    minimumInvest,
+  } = props;
   const [amount, setAmount] = useState("");
   const [loading, setLoading] = useState(false);
 
@@ -14,46 +36,79 @@ export default function JoinPoolDialog(props) {
     setAmount("");
     setOpen(false);
     setLoading(false);
-  }
+  };
 
   const handleSubmit = () => {
     setLoading(true);
-    joinPool(address, amount).then((res) => {
-      handleSuccess(`Joined Pool with ${amount} USD`)
-      handleClose();
-    }).catch((err) => {
-      handleError(err);
-      setLoading(false);
-    })
-  }
+    joinPool(address, amount)
+      .then((res) => {
+        handleSuccess(`Joined Pool with ${amount} USD`);
+        loginCallback();
+        handleClose();
+      })
+      .catch((err) => {
+        handleError(err);
+        setLoading(false);
+      });
+  };
 
   const receivedTokens = ethers.BigNumber.from(usdc(amount)).div(price);
-  const shareOfPool = receivedTokens.mul(100).div(totalSupply.add(receivedTokens));
+  const shareOfPool = receivedTokens
+    .mul(100)
+    .div(totalSupply.add(receivedTokens));
 
   const handleInput = (e) => {
     setAmount(e.target.value);
-  }
+  };
 
-  return(
+  return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle>Join the Pool</DialogTitle>
       <DialogContent>
-        <DialogContentText>You will receive Governance Tokens proportionally to your invest</DialogContentText>
-        <DialogContentText>Price per Governance Token: {toEthString(price, 6)} USD</DialogContentText>
-        <TextField autoFocus type="number" margin="dense" value={amount} onChange={handleInput} label="Invest Amount" placeholder="1" fullWidth inputProps={{min: '0'}} InputProps={{endAdornment: <InputAdornment position="end">USD</InputAdornment>}}/>
-        <DialogContentText>Governance Tokens: {receivedTokens.toString()}</DialogContentText>
-        <DialogContentText>Share of Pool: {shareOfPool.toString()}%</DialogContentText>
+        <DialogContentText>
+          You will receive Governance Tokens proportionally to your invest
+        </DialogContentText>
+        <DialogContentText>
+          Price per Governance Token: {toEthString(price, 6)} USD
+        </DialogContentText>
+        <TextField
+          autoFocus
+          type="number"
+          margin="dense"
+          value={amount}
+          onChange={handleInput}
+          label="Invest Amount"
+          placeholder="1"
+          fullWidth
+          inputProps={{ min: "0" }}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">USD</InputAdornment>,
+          }}
+        />
+        <DialogContentText>
+          Governance Tokens: {receivedTokens.toString()}
+        </DialogContentText>
+        <DialogContentText>
+          Share of Pool: {shareOfPool.toString()}%
+        </DialogContentText>
       </DialogContent>
-      {loading ? 
-        <Stack spacing={2} sx={{textAlign: 'center'}}>
+      {loading ? (
+        <Stack spacing={2} sx={{ textAlign: "center" }}>
           <Typography variant="subtitle1">Joining Pool...</Typography>
           <LinearProgress />
-        </Stack> :
+        </Stack>
+      ) : (
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSubmit} color="success" disabled={minimumInvest.gt(usdc(amount))}>Join</Button>
+          <Button
+            onClick={handleSubmit}
+            color="success"
+            disabled={minimumInvest.gt(usdc(amount))}
+          >
+            Join
+          </Button>
         </DialogActions>
-      }
+      )}
     </Dialog>
-  )
+  );
 }
