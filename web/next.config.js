6c953c5084555ce986e7494d7c3e19f81f1ef60b
Despot Bitschnau <despot@bitschnau.or.at>
2022-04-22 11:41:55 +0200

    Init and config tailwind

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {...Object.fromEntries(Object.entries(process.env).filter(([key]) => !['NODE_VERSION', 'NODE_ENV', '__NEXT_PROCESSED_ENV'].includes(key)))}
-}
+  env: {
+    ...Object.fromEntries(
+      Object.entries(process.env).filter(
+        ([key]) =>
+          ![
+            'NODE_VERSION',
+            'NODE_ENV',
+            '__NEXT_PROCESSED_ENV',
+            'NODE_EXE',
+          ].includes(key)
+      )
+    ),
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
