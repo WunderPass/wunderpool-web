6b3dfe0fdac4a255df98ea9249a6c20b33aa8a39
Moritz Löchner <momo.loechner@gmail.com>
2022-04-28 11:30:05 +0200

    More Options Button that toggles collapsable options

--- web/components/dialogs/newPool.js
+++ web/components/dialogs/newPool.js
@@ -1,15 +1,31 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Input, InputAdornment, InputLabel, LinearProgress, OutlinedInput, Stack, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Collapse,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Divider,
+  Grid,
+  InputAdornment,
+  LinearProgress,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { createPool } from "/services/contract/pools";
 
 export default function NewPoolDialog(props) {
-  const {open, setOpen, fetchPools, handleSuccess, handleError, user} = props;
+  const { open, setOpen, fetchPools, handleSuccess, handleError, user } = props;
   const [poolName, setPoolName] = useState("");
   const [tokenName, setTokenName] = useState("");
   const [tokenSymbol, setTokenSymbol] = useState("");
   const [entryBarrier, setEntryBarrier] = useState("");
   const [value, setValue] = useState("");
   const [waitingForPool, setWaitingForPool] = useState(false);
+  const [showMoreOptions, setShowMoreOptions] = useState(false);
 
   const handleClose = () => {
     setPoolName("");
@@ -19,54 +35,129 @@ export default function NewPoolDialog(props) {
     setValue("");
     setWaitingForPool(false);
     setOpen(false);
-  }
+    setShowMoreOptions(false);
+  };
 
   const handleSubmit = () => {
     setWaitingForPool(true);
-    createPool(poolName, entryBarrier, tokenName, tokenSymbol, value).then((res) => {
-      console.log(res)
-      handleSuccess(`Created Pool "${poolName}"`)
-      handleClose()
-      fetchPools();
-    }).catch((err) => {
-      handleError(err);
-      setWaitingForPool(false);
-    });
-  }
+    console.log(
+      poolName,
+      entryBarrier || value,
+      tokenName || `${poolName} Token`,
+      tokenSymbol || "PGT",
+      value
+    );
+    // createPool(
+    //   poolName,
+    //   entryBarrier || value,
+    //   tokenName || `${poolName} Token`,
+    //   tokenSymbol || "PGT",
+    //   value
+    // )
+    //   .then((res) => {
+    //     console.log(res);
+    //     handleSuccess(`Created Pool "${poolName}"`);
+    //     handleClose();
+    //     fetchPools();
+    //   })
+    //   .catch((err) => {
+    //     handleError(err);
+    //     setWaitingForPool(false);
+    //   });
+  };
 
-  const handleInput = (e) => {
-    setPoolName(e.target.value);
-  }
-
-  return(
+  return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle>New WunderPool</DialogTitle>
       <DialogContent>
         <Stack spacing={2}>
-          <DialogContentText>Create a new WunderPool to Invest with your friends</DialogContentText>
-          <TextField autoFocus label="Pool Name" placeholder="CryptoApes" fullWidth value={poolName} onChange={(e) => setPoolName(e.target.value)}/>
-          <Grid container gap={{xs: 2, sm: 0}}>
-            <Grid item xs={12} sm={9} pr={{xs: 0, sm: 2}}>
-              <TextField label="Governance Token" placeholder={`${poolName}Token`} fullWidth value={tokenName} onChange={(e) => setTokenName(e.target.value)}/>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <TextField label="Symbol" placeholder="TKN" fullWidth value={tokenSymbol} onChange={(e) => setTokenSymbol(e.target.value)}/>
-            </Grid>
-          </Grid>
-          <TextField type="number" value={value} onChange={(e) => setValue(e.target.value)} label="Your Invest" placeholder="1" fullWidth InputProps={{endAdornment: <InputAdornment position="end">USD</InputAdornment>}}/>
-          <TextField type="number" value={entryBarrier} onChange={(e) => setEntryBarrier(e.target.value)} label="Minimum Invest" placeholder="1" fullWidth InputProps={{endAdornment: <InputAdornment position="end">USD</InputAdornment>}}/>
+          <DialogContentText>
+            Create a new WunderPool to Invest with your friends
+          </DialogContentText>
+          <TextField
+            autoFocus
+            label="Pool Name"
+            placeholder="CryptoApes"
+            fullWidth
+            value={poolName}
+            onChange={(e) => setPoolName(e.target.value)}
+          />
+          <TextField
+            type="number"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            label="Your Invest"
+            placeholder="1"
+            fullWidth
+            InputProps={{
+              endAdornment: <InputAdornment position="end">USD</InputAdornment>,
+            }}
+          />
+          <Collapse in={!showMoreOptions} sx={{ marginTop: "0px !important" }}>
+            <Button
+              onClick={() => setShowMoreOptions(true)}
+              sx={{ marginTop: 1 }}
+            >
+              More Options
+            </Button>
+          </Collapse>
+          <Collapse in={showMoreOptions}>
+            <Stack spacing={2}>
+              <Divider />
+              <Grid container gap={{ xs: 2, sm: 0 }}>
+                <Grid item xs={12} sm={9} pr={{ xs: 0, sm: 2 }}>
+                  <TextField
+                    label="Governance Token"
+                    placeholder={`${poolName}Token`}
+                    fullWidth
+                    value={tokenName}
+                    onChange={(e) => setTokenName(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                  <TextField
+                    label="Symbol"
+                    placeholder="TKN"
+                    fullWidth
+                    value={tokenSymbol}
+                    onChange={(e) => setTokenSymbol(e.target.value)}
+                  />
+                </Grid>
+              </Grid>
+              <TextField
+                type="number"
+                value={entryBarrier}
+                onChange={(e) => setEntryBarrier(e.target.value)}
+                label="Minimum Invest"
+                placeholder="1"
+                fullWidth
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">USD</InputAdornment>
+                  ),
+                }}
+              />
+            </Stack>
+          </Collapse>
         </Stack>
       </DialogContent>
-      {waitingForPool ? 
-        <Stack spacing={2} sx={{textAlign: 'center'}}>
+      {waitingForPool ? (
+        <Stack spacing={2} sx={{ textAlign: "center" }}>
           <Typography variant="subtitle1">Creating your Pool...</Typography>
           <LinearProgress />
-        </Stack> :
+        </Stack>
+      ) : (
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSubmit} color="success" disabled={poolName.length < 3 || tokenName.length < 3 || tokenSymbol.length < 3}>Create</Button>
+          <Button
+            onClick={handleSubmit}
+            color="success"
+            disabled={poolName.length < 3}
+          >
+            Create
+          </Button>
         </DialogActions>
-      }
+      )}
     </Dialog>
-  )
+  );
 }
