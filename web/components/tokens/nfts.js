3cfedf7a54c9d40016c70573f552f088005eaffe
Moritz Löchner <momo.loechner@gmail.com>
2022-04-29 17:12:56 +0200

    Sell NFT Button clarification

--- web/components/tokens/nfts.js
+++ web/components/tokens/nfts.js
@@ -8,58 +8,50 @@ export default function NftList(props) {
   const [open, setOpen] = useState(false);
 
   return (
-    <>
-      {nfts.length > 0 ? (
-        <Stack spacing={3} pt={2}>
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Typography variant="h4">NFTs</Typography>
-            <Button onClick={() => setOpen(true)}>Sell Your NFT</Button>
-          </Stack>
-          {nfts.map((nft) => {
-            return (
-              <Paper elevation={3} key={`nft-${nft.address}`} sx={{ p: 2 }}>
-                <Typography variant="h6">{nft.name}</Typography>
-                <Grid container>
-                  {nft.tokens.map((token) => {
-                    return (
-                      <Grid
-                        item
-                        xs={12}
-                        sm={6}
-                        lg={4}
-                        xl={3}
-                        p={1}
-                        key={`nft-${token.uri}`}
-                      >
-                        <Nft
-                          uri={token.uri}
-                          address={nft.address}
-                          tokenId={token.id}
-                          {...props}
-                        />
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-              </Paper>
-            );
-          })}
+    <Stack spacing={3} pt={2}>
+      <Typography variant="h4">NFTs</Typography>
+      {nfts.map((nft) => {
+        return (
+          <Paper elevation={3} key={`nft-${nft.address}`} sx={{ p: 2 }}>
+            <Typography variant="h6">{nft.name}</Typography>
+            <Grid container>
+              {nft.tokens.map((token) => {
+                return (
+                  <Grid
+                    item
+                    xs={12}
+                    sm={6}
+                    lg={4}
+                    xl={3}
+                    p={1}
+                    key={`nft-${token.uri}`}
+                  >
+                    <Nft
+                      uri={token.uri}
+                      address={nft.address}
+                      tokenId={token.id}
+                      {...props}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Paper>
+        );
+      })}
+      <Paper elevation={3} sx={{ p: 2 }}>
+        <Stack spacing={2}>
+          <Typography variant="h5">Do you own an NFT?</Typography>
+          <Typography variant="subtitle1">
+            Then you can sell it to the Pool. If all members agree that you made
+            them a fair offer, they might agree to buy yout NFT
+          </Typography>
+          <Button fullWidth variant="contained" onClick={() => setOpen(true)}>
+            Sell Your NFT
+          </Button>
         </Stack>
-      ) : (
-        <Button
-          variant="contained"
-          sx={{ my: 4 }}
-          fullWidth
-          onClick={() => setOpen(true)}
-        >
-          Sell Your NFT
-        </Button>
-      )}
+      </Paper>
       <SellNftDialog open={open} setOpen={setOpen} {...props} />
-    </>
+    </Stack>
   );
 }
