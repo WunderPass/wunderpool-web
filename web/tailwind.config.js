84419e472c4e34a42f896ccbf8a5cfd3007efa31
Despot Bitschnau <despot@bitschnau.or.at>
2022-05-16 12:26:10 +0200

    INIT, add colors, change first colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         'wunder-blue': '#0096FE',
         'wunder-light-blue': '#01BFFF',
         'wunder-dark-blue': '#020D30',
+        'kaico-blue': '#5F45FD',
+        'kaico-dark-blue': '#462cf1',
+        'kaico-light-blue': '#7560ff',
       },
       backgroundImage: {
         'split-blue-white':
