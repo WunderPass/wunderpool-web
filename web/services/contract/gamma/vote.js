107ba5f3a84fd561fabea35d02ce86671167ab06
Moritz Löchner <momo.loechner@gmail.com>
2022-05-25 17:04:13 +0200

    Legacy functions for GAMMA Pools

---
+++ web/services/contract/gamma/vote.js
@@ -0,0 +1,41 @@
+import useWunderPass from '/hooks/useWunderPass';
+import { initPoolGamma } from '/services/contract/gamma/init';
+import { gasPrice } from '/services/contract/init';
+
+export function voteGamma(poolAddress, proposalId, mode) {
+  return new Promise(async (resolve, reject) => {
+    const { smartContractTransaction } = useWunderPass({
+      name: 'WunderPool',
+      accountId: 'ABCDEF',
+    });
+    const [wunderPool, provider] = initPoolGamma(poolAddress);
+    const tx = await wunderPool.populateTransaction.vote(proposalId, mode, {
+      gasPrice: await gasPrice(),
+    });
+
+    smartContractTransaction(tx).then(async (transaction) => {
+      try {
+        console.log(transaction);
+        const receipt = await provider.waitForTransaction(transaction.hash);
+        resolve(receipt);
+      } catch (error) {
+        reject(error?.error?.error?.error?.message || error);
+      }
+    });
+  });
+}
+
+export function voteForGamma(address, proposalId) {
+  return voteGamma(address, proposalId, 1);
+}
+
+export function voteAgainstGamma(address, proposalId) {
+  return voteGamma(address, proposalId, 2);
+}
+
+export function hasVotedGamma(poolAddress, proposalId, address) {
+  return new Promise(async (resolve, reject) => {
+    const [wunderPool] = initPoolGamma(poolAddress);
+    resolve(await wunderPool.hasVoted(proposalId, address));
+  });
+}
