ae3a0d86a0d2528da8dd357605f22c53f696c6b8
Despot Bitschnau <despot@bitschnau.or.at>
2022-06-02 12:00:40 +0200

    ADD isReady2 in usePool, ADD loading skeleton for proposals, show loading if not loaded, show proposals if loaded, shwo create new ones if no proposals

--- web/components/proposals/list.js
+++ web/components/proposals/list.js
@@ -1,5 +1,14 @@
-import { Box, Paper, Slide, Stack, Tab, Tabs, Typography } from '@mui/material';
-import { useRef, useEffect } from 'react';
+import {
+  Box,
+  Paper,
+  Slide,
+  Stack,
+  Tab,
+  Tabs,
+  Typography,
+  Skeleton,
+} from '@mui/material';
+import { useEffect, useRef } from 'react';
 import { useState } from 'react';
 import ProposalCard from './proposalCard';
 
@@ -7,8 +16,23 @@ export default function ProposalList(props) {
   const { wunderPool, setApe } = props;
   const [tab, setTab] = useState(0);
   const slidingContainer = useRef(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return wunderPool.proposals.length > 0 ? (
+  useEffect(() => {
+    if (wunderPool.isReady2) {
+      console.log(wunderPool.isReady);
+      console.log(wunderPool.proposals);
+      setIsLoading(false);
+    }
+  }, [wunderPool.isReady2]);
+
+  return isLoading ? (
+    <Skeleton
+      variant="rectangular"
+      width="100%"
+      sx={{ height: '100px', borderRadius: 3 }}
+    />
+  ) : wunderPool.proposals.length != 0 ? (
     <Stack spacing={1} style={{ maxWidth: '100%' }}>
       <Tabs value={tab} onChange={(_, val) => setTab(val)}>
         <Tab label="Open" sx={{ maxWidth: 'unset' }} className="flex-1" />
