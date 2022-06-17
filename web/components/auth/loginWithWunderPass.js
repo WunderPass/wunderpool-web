93c5f9e84c11031a9ac3b40901706e4dbcf67b7d
Despot Bitschnau <despot@bitschnau.or.at>
2022-06-17 13:22:16 +0200

    remove git out of lignWithWunderPass

--- web/components/auth/loginWithWunderPass.js
+++ web/components/auth/loginWithWunderPass.js
@@ -65,7 +65,6 @@ export default function LoginWithWunderPass(props) {
             className="fill-white"
             xmlns="http://www.w3.org/2000/svg"
             width="32"
-            git
             height="32"
             version="1"
             viewBox="0 0 2000 1917"
