20fbbf98c74d6ef94587a8a763195f0babe46b96
Despot Bitschnau <despot@bitschnau.or.at>
2022-06-17 13:14:20 +0200

    adding unsaved features

--- web/components/auth/loginWithWunderPass.js
+++ web/components/auth/loginWithWunderPass.js
@@ -65,6 +65,7 @@ export default function LoginWithWunderPass(props) {
             className="fill-white"
             xmlns="http://www.w3.org/2000/svg"
             width="32"
+            git
             height="32"
             version="1"
             viewBox="0 0 2000 1917"
