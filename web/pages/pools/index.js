e4c1c503df8cc6229e613865bb0e504b8efefbce
Despot Bitschnau <despot@bitschnau.or.at>
2022-06-30 14:08:03 +0200

    fix broken server

--- web/pages/pools/index.js
+++ web/pages/pools/index.js
@@ -1,11 +1,132 @@
 import NewPoolDialog from '/components/dialogs/newPool';
 import BalanceBox from '/components/pool/balanceBox';
+import { displayWithDecimalPlaces, currency } from '/services/formatter';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MdContentCopy } from 'react-icons/md';
+import { PieChart } from 'react-minimal-pie-chart';
+import { MdGroups } from 'react-icons/md';
 import Head from 'next/head';
-import PoolList from './poolList';
-import { Container, Pagination, Skeleton, Typography } from '@mui/material';
+import Link from 'next/link';
+import {
+  Container,
+  Pagination,
+  Paper,
+  Skeleton,
+  Typography,
+} from '@mui/material';
+import InitialsAvatar from '/components/utils/initialsAvatar';
+
+function PoolCard(props) {
+  const { pool } = props;
+  const members = pool.members;
+
+  return (
+    <Link href={`/pools/${pool.address}?name=${pool.name}`} passHref>
+      <Paper
+        className="container-white mb-4 pb-6 sm:pb-0 cursor-pointer lg:mb-0 sm:mb-6"
+        elevation={1}
+        sx={{ p: 2 }}
+      >
+        <div className="flex flex-col">
+          <div className="flex flex-row justify-between items-center">
+            <Typography className="text-md">{pool.name}</Typography>
+            <div className="bg-white hover:bg-[#ededed]  rounded-md border-2 border-kaico-extra-light-blue p-5 text-md font-semibold cursor-pointer"></div>
+          </div>
+          <Typography className="text-lg pt-3 font-semibold">
+            {currency(pool.totalBalance, {})}
+          </Typography>
+          <div className="flex flex-row justify-between items-center pb-4">
+            <div className="flex flex-row justify-start items-center ">
+              <PieChart
+                className="w-8 sm:w-6 my-1 mt-6"
+                data={[
+                  {
+                    title: 'One',
+                    value: 100 - parseInt(pool.userShare),
+                    color: '#E4DFFF',
+                  },
+                  {
+                    title: 'Two',
+                    value: parseInt(pool.userShare),
+                    color: '#5F45FD',
+                  },
+                ]}
+              />
+
+              <Typography className="text-md  pt-5 pl-3">
+                {pool.userShare &&
+                  `${parseInt(pool.userShare)}% (${currency(
+                    pool.userBalance,
+                    {}
+                  )})`}
+              </Typography>
+            </div>
+
+            <div className="flex flex-row   mt-4">
+              <div className="flex flex-row">
+                {members &&
+                  members
+                    .sort((a, b) => b.share - a.share)
+                    .slice(0, 3)
+                    .map((member, i) => {
+                      return (
+                        <InitialsAvatar
+                          key={`member-${i}`}
+                          tooltip={`${
+                            member.wunderId || 'External User'
+                          }: ${parseInt(member.share)}%`}
+                          text={member.wunderId ? member.wunderId : '0-X'}
+                          separator="-"
+                          color={['green', 'blue', 'red', 'teal'][i % 4]}
+                        />
+                      );
+                    })}
+              </div>
+              {members && members.length > 3 && (
+                <div className="flex flex-row">
+                  <Typography className="pt-2.5 pl-2 ">+</Typography>
+                  <Typography className="pt-2.5 pl-2 ">
+                    {members.length - 3}
+                  </Typography>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </Paper>
+    </Link>
+  );
+}
+
+function PoolList(props) {
+  const { pools, setOpen } = props;
+
+  return pools.length > 0 ? (
+    <div className="lg:grid lg:grid-cols-2 lg:gap-6 w-full">
+      {pools.map((pool, i) => {
+        return <PoolCard key={`pool-card-${i}`} pool={pool} />;
+      })}
+    </div>
+  ) : (
+    <div className="container-white">
+      <div className="flex flex-col items-center ">
+        <div className="border-solid text-kaico-blue rounded-full bg-kaico-extra-light-blue p-5 my-2 mt-6 mb-4">
+          <MdGroups className="text-4xl" />
+        </div>
+        <Typography className="my-2 mb-10" variant="h7">
+          No Pools joined yet
+        </Typography>
+        <button
+          className="btn-kaico-white items-center w-full my-5 py-3 px-3 mb-8 text-md "
+          onClick={() => setOpen(true)}
+        >
+          Create pool
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default function Pools(props) {
   const { user, handleSuccess } = props;
@@ -32,7 +153,6 @@ export default function Pools(props) {
                 <Typography className=" text-2xl my-5 sm:text-4xl">
                   Hello {user?.wunderId},
                 </Typography>
-
                 <div className=" border-solid text-kaico-blue truncate rounded-lg bg-gray-300 p-3 ">
                   <CopyToClipboard
                     text={user?.address}
