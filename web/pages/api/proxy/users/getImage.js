88c313d94c13736df8cf22f0488d27fd85272598
Despot <despot@bitschnau.or.at>
2022-07-15 16:36:34 +0200

    update user profile picture

---
+++ web/pages/api/proxy/users/getImage.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const http = require('https');
+
+export default function handler(req, res) {
+    const options = {
+        hostname: 'identity-service.wunderpass.org',
+        path: `/v4/wunderPasses/${req.query.wunderId}/image`,
+        headers: {
+            authorization: `Bearer ${process.env.IS_SERVICE_TOKEN}`,
+        },
+    };
+    http.get(options, function (resp) {
+        if (resp.statusCode == 200 && resp.headers['content-type']) {
+            res
+                .status(200)
+                .setHeader('content-type', resp.headers['content-type'])
+                .setHeader('content-length', resp.headers['content-length'])
+                .send(resp);
+            return;
+        } else {
+
+            const file = fs.createReadStream(
+                './public/blank-profile-pic.png'
+            );
+            res.status(200).setHeader('content-type', 'image/png').send(file);
+        }
+    });
+}
