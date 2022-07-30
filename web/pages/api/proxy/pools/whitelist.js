4a4264969c38cb3cd65b32e06da2ab51cee82ec9
Moritz Löchner <momo.loechner@gmail.com>
2022-07-30 18:40:58 +0200

    tvl metrics besser machen

--- web/pages/api/proxy/pools/whitelist.js
+++ web/pages/api/proxy/pools/whitelist.js
@@ -51,7 +51,7 @@ export default async function handler(req, res) {
     });
     res.status(200).json(response.data);
   } catch (error) {
-    console.log(error);
+    console.log(error?.toJSON() || error);
     res.status(500).json(error);
   }
 }
