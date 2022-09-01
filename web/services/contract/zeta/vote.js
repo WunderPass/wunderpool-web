67a2a01b7d6cacff1f37331bcdd85b42c0a8d696
Despot Bitschnau <despot@bitschnau.or.at>
2022-09-01 14:54:17 +0200

    remove zeta

--- web/services/contract/zeta/vote.js
+++
@@ -1,8 +0,0 @@
-import { initProposalEpsilon } from '/services/contract/epsilon/init';
-
-export function hasVotedEpsilon(poolAddress, proposalId, address) {
-  return new Promise(async (resolve, reject) => {
-    const [wunderProposal] = initProposalEpsilon();
-    resolve(await wunderProposal.hasVoted(poolAddress, proposalId, address));
-  });
-}
