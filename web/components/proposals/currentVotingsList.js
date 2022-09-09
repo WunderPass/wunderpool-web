ffa698aaefbaf675f96ee49c3eedc44165d883cb
Despot B <despot.b@hotmail.com>
2022-09-09 13:32:03 +0200

    console log

--- web/components/proposals/currentVotingsList.js
+++ web/components/proposals/currentVotingsList.js
@@ -26,6 +26,9 @@ export default function CurrentVotingsList(props) {
         )
         .sort((one, two) => new Date(two.createdAt) - new Date(one.createdAt))
         .map((proposal) => {
+          {
+            console.log('Proposal:', proposal);
+          }
           return (
             <ProposalCard
               key={`proposal-${proposal.id}`}
