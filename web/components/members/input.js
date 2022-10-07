59d49b000e32a1b8603183569ff88b699a82ff23
Despot B <despot.b@hotmail.com>
2022-10-07 15:32:21 +0200

    update createPool tests with ids

--- web/components/members/input.js
+++ web/components/members/input.js
@@ -39,57 +39,60 @@ export default function MemberInput({
   };
 
   return (
-    <Autocomplete
-      className="w-full text-gray-700 my-4 leading-tight rounded-lg bg-[#F6F6F6] focus:outline-none"
-      multiple={multiple}
-      loading={loading}
-      options={inputTrue ? members : friends}
-      value={selectedMembers}
-      onChange={handleChange}
-      onInputChange={handleInput}
-      filterOptions={filterOptions}
-      isOptionEqualToValue={(option, val) => option.address == val.address}
-      getOptionLabel={(option) => getNameFor(option)}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          className="w-full text-gray-700 leading-tight rounded-lg bg-[#F6F6F6] focus:outline-none"
-          label="WunderPass Users"
-          placeholder="Search your Friends..."
-        />
-      )}
-      renderOption={(props, option) => {
-        return (
-          <li {...props}>
-            <Stack
-              direction="row"
-              alignItems="center"
-              spacing={2}
-              sx={{ width: '100%' }}
-            >
-              <Avatar wunderId={option.handle} text={option.handle} />
-              <div>
-                <Typography
-                  variant="subtitle1"
-                  whiteSpace="nowrap"
-                  textOverflow="ellipsis"
-                  overflow="hidden"
-                >
-                  {getNameFor(option)}
-                </Typography>
-                <Typography
-                  variant="subtitle2"
-                  whiteSpace="nowrap"
-                  textOverflow="ellipsis"
-                  overflow="hidden"
-                >
-                  {option.email}
-                </Typography>
-              </div>
-            </Stack>
-          </li>
-        );
-      }}
-    />
+    <>
+      {console.log('inputTrue', inputTrue)}
+      <Autocomplete
+        className="w-full text-gray-700 my-4 leading-tight rounded-lg bg-[#F6F6F6] focus:outline-none"
+        multiple={multiple}
+        loading={loading}
+        options={inputTrue ? members : friends}
+        value={selectedMembers}
+        onChange={handleChange}
+        onInputChange={handleInput}
+        filterOptions={filterOptions}
+        isOptionEqualToValue={(option, val) => option.address == val.address}
+        getOptionLabel={(option) => getNameFor(option)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            className="w-full text-gray-700 leading-tight rounded-lg bg-[#F6F6F6] focus:outline-none"
+            label="WunderPass Users"
+            placeholder="Search your Friends..."
+          />
+        )}
+        renderOption={(props, option) => {
+          return (
+            <li {...props}>
+              <Stack
+                direction="row"
+                alignItems="center"
+                spacing={2}
+                sx={{ width: '100%' }}
+              >
+                <Avatar wunderId={option.handle} text={option.handle} />
+                <div>
+                  <Typography
+                    variant="subtitle1"
+                    whiteSpace="nowrap"
+                    textOverflow="ellipsis"
+                    overflow="hidden"
+                  >
+                    {getNameFor(option)}
+                  </Typography>
+                  <Typography
+                    variant="subtitle2"
+                    whiteSpace="nowrap"
+                    textOverflow="ellipsis"
+                    overflow="hidden"
+                  >
+                    {option.email}
+                  </Typography>
+                </div>
+              </Stack>
+            </li>
+          );
+        }}
+      />
+    </>
   );
 }
