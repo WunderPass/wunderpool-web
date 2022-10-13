eb970387cfb0a7750dc88afce89439d8ce30e0e3
Despot B <despot.b@hotmail.com>
2022-10-13 18:42:39 +0200

    update transak in balancebox

--- web/components/dialogs/topUpAlert.js
+++ web/components/dialogs/topUpAlert.js
@@ -19,6 +19,8 @@ import { RiVisaFill } from 'react-icons/ri';
 import { SiMastercard } from 'react-icons/si';
 import { FaGooglePay } from 'react-icons/fa';
 import { FaCcPaypal } from 'react-icons/fa';
+import TransakLogo from '/public/transak.png';
+import Image from 'next/image';
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -26,8 +28,13 @@ const Transition = forwardRef(function Transition(props, ref) {
 
 export default function TopUpAlert(props) {
   const { open, setOpen, user } = props;
+  const [isApple, setIsApple] = useState(false);
   const [redirectUrl, setRedirectUrl] = useState(null);
   const [checked, setChecked] = useState(null);
+
+  useEffect(() => {
+    setIsApple(window.navigator.vendor.toLowerCase().match(/apple/));
+  }, []);
 
   const handleSuccess = ({ wunderId, address, loginMethod }) => {
     user.updateLoginMethod(loginMethod);
@@ -57,7 +64,7 @@ export default function TopUpAlert(props) {
       <DialogContent>
         {user.loginMethod == 'WunderPass' ? (
           <Stack spacing={4} className="flex w-full ">
-            <Alert severity={user.usdBalance > 1 ? 'info' : 'warning'}>
+            <div className="container-casama">
               <span className="font-bold">Buy </span>
               <span className="">or </span>
               <span className="font-bold">Sell </span>
@@ -65,7 +72,8 @@ export default function TopUpAlert(props) {
                 your USDC for fiat money directly with Transak. Supports SEPA,
                 Visa, Mastercard, ApplePay and GooglePay.
               </span>
-              <div className="flex flex-col sm:flex-row items-center justify-center my-5 ">
+              {/* DESKTOP VIEW  */}
+              <div className="hidden sm:flex flex-row items-center justify-center my-5 ">
                 <Chip
                   className="items-center p-2 my-1 flex bg-casama-extra-light-blue text-casama-blue  mr-4"
                   size="medium"
@@ -86,29 +94,87 @@ export default function TopUpAlert(props) {
                   size="medium"
                   label={<FaGooglePay className="text-5xl" />}
                 />
-
                 <Chip
-                  className="items-center p-2 my-1 flex bg-casama-extra-light-blue text-casama-blue  mr-4"
+                  className={
+                    isApple
+                      ? 'items-center p-2 my-1 flex bg-casama-extra-light-blue text-casama-blue  mr-4'
+                      : 'hidden'
+                  }
                   size="medium"
                   label={<BsApple className="text-2xl" />}
                 />
+              </div>
+              {/* MOBILE VIEW */}
+              <div className="sm:hidden flex flex-col items-center justify-between  my-5 ">
+                <div className="flex flex-row items-center justify-between ">
+                  <Chip
+                    className=" p-2 my-1 flex bg-casama-extra-light-blue text-casama-blue  mr-5"
+                    size="medium"
+                    label={<RiVisaFill className="text-4xl" />}
+                  />
+                  <Chip
+                    className=" p-2  my-1 flex bg-casama-extra-light-blue text-casama-blue  "
+                    size="medium"
+                    label={<SiMastercard className="text-4xl" />}
+                  />
+                </div>
+                <div className="flex flex-row items-center justify-between ">
+                  <Chip
+                    className=" items-center my-1 p-2 flex bg-casama-extra-light-blue text-casama-blue mr-3 "
+                    size="medium"
+                    label={<FaGooglePay className="text-5xl" />}
+                  />
+                  <Chip
+                    className="items-center p-2 my-1 flex bg-casama-extra-light-blue text-casama-blue  "
+                    size="medium"
+                    label={<SiSepa className="text-5xl" />}
+                  />
+                </div>
+                <div className="flex flex-row">
+                  <Chip
+                    className={
+                      isApple
+                        ? 'items-center p-2 my-1 flex bg-casama-extra-light-blue text-casama-blue  mr-4'
+                        : 'hidden'
+                    }
+                    size="medium"
+                    label={<BsApple className="text-2xl" />}
+                  />
+                </div>
               </div>
               <a
                 href={`${process.env.TRANSAK_URL}${process.env.TRANSAK_API_KEY}`}
                 target="_blank"
               >
-                <button className="btn btn-info w-full">Use Transak</button>
+                <button className="btn-casama-white p-2 w-full">
+                  Use Transak
+                </button>
               </a>
-            </Alert>
+            </div>
 
-            <Alert severity={user.usdBalance > 1 ? 'info' : 'warning'}>
+            <div className="container-casama">
               Manage your wallet in WunderPass and decide which provider you
               want to use there.
-              <div className="flex flex-row items-center justify-center my-5  ">
+              <div className="flex flex-row items-center justify-between sm:justify-center my-5 ">
                 <Chip
-                  className="items-center p-2 my-1 sm:flex bg-casama-extra-light-blue text-casama-blue  mr-4"
+                  className="items-center p-2 my-1 sm:flex bg-casama-extra-light-blue text-casama-blue  sm:mr-5"
                   size="medium"
                   label={<FaCcPaypal className="text-3xl" />}
+                />
+                <Chip
+                  className="items-center  sm:flex bg-casama-extra-light-blue text-casama-blue  "
+                  size="medium"
+                  label={
+                    <div className="pt-1.5">
+                      <Image
+                        src={TransakLogo}
+                        alt="TransakLogo"
+                        layout="intrinsic"
+                        width={100}
+                        height={30}
+                      />
+                    </div>
+                  }
                 />
               </div>
               <a
@@ -116,11 +182,11 @@ export default function TopUpAlert(props) {
                 href={`${process.env.WUNDERPASS_URL}/balance/topUp?redirectUrl=${redirectUrl}`}
                 target="_blank"
               >
-                <button className="btn btn-info w-full">
+                <button className="btn-casama-white p-2 w-full">
                   Go to WunderPass
                 </button>
               </a>
-            </Alert>
+            </div>
 
             {user.usdBalance < 1 && !user.checkedTopUp && (
               <div className="flex flex-row justify-start items-center mt-2">
