c4f8452cc7c5b0fb9bc61a4018dccd965a9199b0
Despot B <despot.b@hotmail.com>
2022-11-03 22:16:44 +0100

    create dropdown component

--- web/pages/betting/bets.js
+++ web/pages/betting/bets.js
@@ -2,23 +2,11 @@ import { useRouter } from 'next/router';
 import CustomHeader from '/components/general/utils/customHeader';
 import BettingBox from '/components/betting/pool/bettingBox';
 import { useEffect, useState } from 'react';
-import {
-  Container,
-  Menu,
-  MenuItem,
-  Skeleton,
-  Typography,
-  Chip,
-  Divider,
-} from '@mui/material';
-import AdvancedPoolDialog from '/components/betting/dialogs/advancedPool/dialog';
-import QuickPoolDialog from '/components/betting/dialogs/quickPool/dialog';
+import { Container, Skeleton, Typography } from '@mui/material';
 import BetsList from '/components/betting/dashboard/betsList';
-import { MdKeyboardArrowDown } from 'react-icons/md';
-import MenuUnstyled from '@mui/base/MenuUnstyled';
-import MenuItemUnstyled from '@mui/base/MenuItemUnstyled';
 import axios from 'axios';
 import useBettingService from '/hooks/useBettingService';
+import DropDown from '/components/general/utils/dropDown';
 
 export default function Bets(props) {
   const { user, handleInfo, handleError } = props;
@@ -26,14 +14,7 @@ export default function Bets(props) {
   const [eventTypes, setEventTypes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [eventTypeSort, setEventTypeSort] = useState('All Events');
-  const [eventListOpen, setEventListOpen] = useState(null);
-  const [open, setOpen] = useState(false);
   const bettingService = useBettingService(user.address, handleError);
-
-  const handleMenuClose = () => {
-    setOpen(false);
-    setEventListOpen(null);
-  };
 
   const determineEventTypes = () => {
     let eventTypes = events.map((event) => event.competitionName);
@@ -107,53 +88,11 @@ export default function Bets(props) {
                     My Bets
                   </Typography>
 
-                  <button
-                    className={
-                      eventListOpen
-                        ? 'container-white-p-0 p-3 border-gray-800 border w-56'
-                        : 'container-white-p-0 p-3 border-gray-300 border w-56'
-                    }
-                    onClick={(e) => setEventListOpen(e.currentTarget)}
-                  >
-                    <div className="flex flex-row justify-between items-center">
-                      <p className="text-gray-800 text-lg font-medium ml-2">
-                        {eventTypeSort}
-                      </p>
-                      <MdKeyboardArrowDown className="text-2xl text-gray-800" />
-                    </div>
-                  </button>
-
-                  <MenuUnstyled
-                    className="container-white-p-0 rounded-full m-2 drop-shadow-around"
-                    open={Boolean(eventListOpen)}
-                    onClose={handleMenuClose}
-                    onClick={handleMenuClose}
-                    anchorEl={eventListOpen}
-                  >
-                    {eventTypes &&
-                      eventTypes.map((eventType, i) => {
-                        return (
-                          <MenuItemUnstyled
-                            className={
-                              i == 0
-                                ? 'shadow-dropdown-b border-b border-gray-200 hover:border-gray-400 hover:drop-shadow-md mt-1 w-56'
-                                : i == eventType.length - 1
-                                ? 'shadow-dropdown-t border-t border-gray-200 hover:border-gray-400 hover:drop-shadow-md mb-1 w-56'
-                                : 'shadow-dropdown-y border-y border-gray-200 hover:border-gray-400 hover:drop-shadow-md w-56'
-                            }
-                          >
-                            <div className="flex flex-row items-center justify-between w-full">
-                              <button
-                                onClick={() => setEventTypeSort(eventType)}
-                                className="text-gray-800 text-base text-left font-medium w-full p-3 ml-1 mt-1 cursor-pointer"
-                              >
-                                {eventType}
-                              </button>
-                            </div>
-                          </MenuItemUnstyled>
-                        );
-                      })}
-                  </MenuUnstyled>
+                  <DropDown
+                    list={eventTypes}
+                    value={eventTypeSort}
+                    setValue={setEventTypeSort}
+                  />
                 </div>
 
                 {!loading ? (
