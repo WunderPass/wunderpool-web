1bd4c3b8a9fc53645a00b32d67bf0401ed1810a0
Moritz Löchner <momo.loechner@gmail.com>
2022-11-14 11:41:24 +0100

    Remove WunderPass Button from switchChain Alert

--- web/components/general/dialogs/switchChainAlert.js
+++ web/components/general/dialogs/switchChainAlert.js
@@ -3,14 +3,11 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
-  Divider,
   Slide,
   Stack,
-  Typography,
 } from '@mui/material';
 import Image from 'next/image';
 import { forwardRef } from 'react';
-import CreateYourWunderPass from '/components/general/auth/createYourWunderPass';
 import MetaMaskLogo from '/components/general/utils/metaMaskLogo';
 import WalletConnectIcon from '/public/images/walletconnect.png';
 
@@ -20,12 +17,6 @@ const Transition = forwardRef(function Transition(props, ref) {
 
 export default function SwitchChainAlert({ user }) {
   const { unsupportedChain, loginMethod } = user;
-
-  const handleSuccess = ({ wunderId, address, loginMethod }) => {
-    user.updateLoginMethod(loginMethod);
-    user.updateWunderId(wunderId);
-    user.updateAddress(address);
-  };
 
   const renderTitleImage = () => {
     if (loginMethod == 'MetaMask') {
@@ -59,17 +50,6 @@ export default function SwitchChainAlert({ user }) {
           <Alert severity="info">
             Please Switch Chain to Polygon to Proceed
           </Alert>
-          <Divider />
-          <Typography textAlign="center">
-            Or use Casama with WunderPass to avoid these kind of issues
-          </Typography>
-          <CreateYourWunderPass
-            text="Login With WunderPass"
-            name="Casama"
-            redirect={'pools'}
-            intent={['wunderId', 'address']}
-            onSuccess={handleSuccess}
-          />
         </Stack>
       </DialogContent>
     </Dialog>
