68f4f18d3974b26ee5cecfaf791bdb1038c64779
Moritz Löchner <momo.loechner@gmail.com>
2022-11-30 21:54:00 +0100

    Correct for sponsored Pools in stats

--- web/pages/api/betting/admin/statsHistory.js
+++ web/pages/api/betting/admin/statsHistory.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
+import axios from 'axios';
 
 export default async function handler(req, res) {
   try {
     let stats = [];
-    if (fs.existsSync('./data/bettingStats.json')) {
-      stats = JSON.parse(fs.readFileSync('./data/bettingStats.json', 'utf8'));
+    if (process.env.NODE_ENV == 'development') {
+      stats = (
+        await axios({
+          url: 'https://app.casama.io/api/betting/admin/statsHistory',
+        })
+      ).data;
+    } else {
+      if (fs.existsSync('./data/bettingStats.json')) {
+        stats = JSON.parse(fs.readFileSync('./data/bettingStats.json', 'utf8'));
+      }
     }
 
     res.status(200).json(stats);
