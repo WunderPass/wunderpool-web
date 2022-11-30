0e20ce91d5fbacd50061607ff62b48a05b1ec429
Moritz Löchner <momo.loechner@gmail.com>
2022-11-30 21:32:02 +0100

    Faster Loading Times and ISO Code in betting Helper

--- web/components/betting/events/eventCard/predictionInput.js
+++ web/components/betting/events/eventCard/predictionInput.js
@@ -1,6 +1,3 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
 export default function EventCardPredicitionInput(props) {
   const {
     event,
@@ -12,39 +9,6 @@ export default function EventCardPredicitionInput(props) {
     color = 'text-casama-blue',
   } = props;
 
-  const [homeCountryIso, setHomeCountryIso] = useState(null);
-  const [awayCountryIso, setAwayCountryIso] = useState(null);
-
-  //TODO ASK MORITZ HOW HE WOULD SOLVE THIS SHIT
-  const fetchCountryHomeIso = async (country) => {
-    try {
-      const { data } = await axios({
-        url: `/api/betting/metadata/countryIso?name=${country}`,
-        // params: { query },
-      });
-      setHomeCountryIso(data);
-    } catch (error) {
-      console.log('error', 'gukus');
-    }
-  };
-
-  const fetchCountryAwayIso = async (country) => {
-    try {
-      const { data } = await axios({
-        url: `/api/betting/metadata/countryIso?name=${country}`,
-        // params: { query },
-      });
-      setAwayCountryIso(data);
-    } catch (error) {
-      console.log('error', 'gukus');
-    }
-  };
-
-  useEffect(() => {
-    fetchCountryHomeIso(event?.teamHome?.name);
-    fetchCountryAwayIso(event?.teamAway?.name);
-  }, []);
-
   return (
     <>
       <div className="flex items-center justify-center mt-4">
@@ -53,11 +17,7 @@ export default function EventCardPredicitionInput(props) {
 
       <div className="flex flex-row justify-between w-full mb-3">
         <div className="w-full flex flex-col items-center justify-center">
-          <div>
-            {homeCountryIso
-              ? homeCountryIso
-              : (event?.teamHome?.name).substr(0, 3)}
-          </div>
+          <div>{event?.teamHome?.shortName}</div>
 
           <div className="w-20">
             <input
@@ -71,11 +31,7 @@ export default function EventCardPredicitionInput(props) {
           </div>
         </div>
         <div className="w-full flex flex-col items-center justify-center">
-          <div>
-            {awayCountryIso
-              ? awayCountryIso
-              : (event?.teamAway?.name).substr(0, 3)}
-          </div>
+          <div>{event?.teamAway?.shortName}</div>
           <div className="w-20">
             <input
               togglable="false"
