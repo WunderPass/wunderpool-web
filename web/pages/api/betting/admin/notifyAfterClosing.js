5acddc9b885b645ae1abc9d74ef07d349a27e311
Moritz Löchner <momo.loechner@gmail.com>
2022-12-01 22:50:20 +0100

    NotifyAfterClosing mit FirstName

--- web/pages/api/betting/admin/notifyAfterClosing.js
+++ web/pages/api/betting/admin/notifyAfterClosing.js
@@ -49,10 +49,12 @@ export default async function handler(req, res) {
     const users = await getUsersByAddresses(
       competition.members.map((m) => m.address)
     );
+
     const members = competition.members.map((m) => {
       const user = users.find((u) => compAddr(u.wallet_address, m.address));
       return {
         email: user?.email,
+        firstName: user?.firstname,
         profileName: m.profileName,
         userName: m.userName,
         prediction: competition.games[0].participants.find((p) =>
@@ -63,11 +65,10 @@ export default async function handler(req, res) {
     });
 
     const winners = members.filter((m) => m.email && m.profit > 0);
-
     winners.map((user) => {
       return sendCompetitionEndedMail({
         to: user.email,
-        firstName: user.firstname || user.handle,
+        firstName: user.firstName || user.handle,
         teamHome: competition.games[0].event.teamHome,
         teamAway: competition.games[0].event.teamAway,
         outcome: competition.games[0].event.outcome,
@@ -79,7 +80,7 @@ export default async function handler(req, res) {
       './data/notifiedAfterClosing.json',
       JSON.stringify(notified)
     );
-    res.status(200).send('Notified Users');
+    res.status(200).send(winners);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
