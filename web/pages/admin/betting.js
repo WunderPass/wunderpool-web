c114fd3f11839508d9cedf3a9193406afae80083
Moritz Löchner <momo.loechner@gmail.com>
2022-12-05 23:39:06 +0100

    Separate Compenent

--- web/pages/admin/betting.js
+++ web/pages/admin/betting.js
@@ -21,7 +21,7 @@ import fs from 'fs';
 import ParticipantTable from '../../components/betting/games/ParticipantTable';
 import { calculateWinnings } from '../../services/bettingHelpers';
 import { FiShare } from 'react-icons/fi';
-import { toPng } from 'html-to-image';
+import ShareCompetitionCard from '../../components/betting/dashboard/shareCompetitionCard';
 
 function TimeFrame({ start, end }) {
   const startHour = String(start.getHours()).padStart(2, '0');
@@ -315,7 +315,6 @@ function ClosedCompetitionCard({
   const [showMembers, setShowMembers] = useState(false);
   const [screenshotMode, setScreenshotMode] = useState(false);
   const [loading, setLoading] = useState(false);
-  const screenshotRef = useRef(null);
 
   const {
     stake,
@@ -364,118 +363,15 @@ function ClosedCompetitionCard({
     setScreenshotMode(true);
   };
 
-  useEffect(async () => {
-    if (!screenshotMode) return;
-    if (screenshotRef.current === null) {
-      return;
-    }
-    try {
-      const dataUrl = await toPng(screenshotRef.current, {
-        includeQueryParams: true,
-      });
-      if (navigator.share) {
-        const imageObj = new File(
-          [await (await fetch(dataUrl)).arrayBuffer()],
-          'results.png',
-          { type: 'image/png' }
-        );
-        navigator.share({
-          files: [imageObj],
-        });
-      } else {
-        const link = document.createElement('a');
-        link.download = 'results.png';
-        link.href = dataUrl;
-        link.click();
-      }
-    } catch (error) {
-      handleError(error);
-      console.log(error);
-    }
-    setScreenshotMode(false);
-  }, [screenshotMode, screenshotRef.current]);
-
   return (
     <>
-      <div
-        ref={screenshotRef}
-        className={`p-3 bg-gray-100 flex-col justify-around gap-3 w-full max-w-md aspect-[9/16] ${
-          screenshotMode ? 'flex' : 'hidden'
-        }`}
-      >
-        <img src="/casama.svg" className="w-2/3 mx-auto" />
-        <div className="flex flex-col gap-3">
-          <h1 className="font-semibold text-center text-lg">{name}</h1>
-          <div className="flex flex-col w-full ml-2">
-            {/* ICONS */}
-            <div className="flex flex-row justify-between items-center text-center w-full">
-              <div className="flex flex-col justify-center items-center text-center w-5/12">
-                <img
-                  src={`/api/betting/events/teamImage?id=${firstGame.event.teamHome.id}`}
-                  className="w-20 mb-2"
-                />
-              </div>
-              <p className="text-3xl font-semibold">vs</p>
-              <div className="flex flex-col justify-center items-center text-center w-5/12">
-                <img
-                  src={`/api/betting/events/teamImage?id=${firstGame.event.teamAway.id}`}
-                  className="w-20 mb-2"
-                />
-              </div>
-            </div>
-
-            {/* NAMEN */}
-            <div className="flex flex-row justify-between items-center text-center mb-2 w-full">
-              <div className="flex flex-row justify-center items-center text-center w-5/12">
-                <p className="text-xl sm:text-2xl font-semibold">
-                  {firstGame.event.teamHome?.name || firstGame.event?.teamHome}
-                </p>
-              </div>
-              <div className="flex flex-col justify-center items-center text-center w-5/12">
-                <p className="text-xl sm:text-2xl font-semibold">
-                  {firstGame.event.teamAway?.name || firstGame.event?.teamAway}
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="rounded-xl bg-casama-blue text-white w-full flex flex-col justify-center items-center">
-            <p className="mb-4 sm:mb-5 pb-1 sm:pb-2 mt-1 text-xl sm:text-2xl font-medium border-b border-gray-400 w-11/12 text-center">
-              Result
-            </p>
-            <div className="flex flex-row justify-center items-center w-full mb-3">
-              <p className="w-5/12 text-center text-base sm:text-xl px-2">
-                {firstGame.event.teamHome.name}
-              </p>
-
-              <div className="w-2/12 flex flex-row justify-center">
-                <p className="font-semibold text-xl sm:text-2xl">
-                  {firstGame.event?.outcome[0] || 0}
-                </p>
-                <p className="px-1 text-xl sm:text-2xl">:</p>
-                <p className="font-semibold text-xl sm:text-2xl">
-                  {firstGame.event?.outcome[1] || 0}
-                </p>
-              </div>
-              <p className="w-5/12 text-center text-base sm:text-xl px-2">
-                {firstGame.event.teamAway.name}
-              </p>
-            </div>
-          </div>
-          <ParticipantTable
-            participants={gameResults}
-            stake={sponsored ? 0 : stake}
-            user={user}
-            hideImages
-            hideLoosers
-            headerText="Winners"
-          />
-        </div>
-        <div className="flex justify-center">
-          <h6 className="text-xl text-center text-casama-blue font-bold bg-white p-3 rounded-xl whitespace-nowrap">
-            🔗 app.casama.io
-          </h6>
-        </div>
-      </div>
+      <ShareCompetitionCard
+        competition={competition}
+        screenshotMode={screenshotMode}
+        setScreenshotMode={setScreenshotMode}
+        handleError={handleError}
+        user={user}
+      />
       <Paper className="p-3 my-2 rounded-xl relative">
         {members.length > 0 && (
           <div className="absolute top-3 right-3 flex flex-col items-end gap-2">
