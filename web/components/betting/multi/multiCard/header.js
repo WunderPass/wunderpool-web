1e3eab75fce95a3858f1877cedd6d2a30ad37828
Despot B <despot.b@hotmail.com>
2023-01-13 11:48:31 +0100

    add logos to preview

--- web/components/betting/multi/multiCard/header.js
+++ web/components/betting/multi/multiCard/header.js
@@ -7,7 +7,22 @@ export default function MultitCardHeader(props) {
         <span>{competition.name}</span>{' '}
         <span>({competition.games.length} games in competition)</span>
       </div>
-      <div className="flex flex-row justify-between items-center"></div>
+      <div className="flex flex-row justify-between items-center mt-16">
+        {competition.games.map((game) => {
+          return (
+            <div className="flex flex-col sm:flex-row ">
+              <img
+                src={`/api/betting/events/teamImage?id=${game.event?.teamHome?.id}`}
+                className="w-16 drop-shadow-[0_0_18px_rgba(0,0,0,0.15)] "
+              />
+              <img
+                src={`/api/betting/events/teamImage?id=${game.event?.teamAway?.id}`}
+                className="w-16 drop-shadow-[0_0_18px_rgba(0,0,0,0.15)]"
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
